ccdd7af7618d073175dcf89e9156c43f5d692a76
helliotroa <helliotroa@cihologramas.com>
2023-11-17 16:38:32 -0500

    Anotaciones interesantes en miles-km. Se crea directorio para las tareas

---
+++ homework/mistake-week1.c
@@ -0,0 +1,13 @@
+/*
+    Debugging the first C program
+    Johann Roa
+    November, 2023
+*/
+
+#include<stdio.h>
+
+int main(void)
+{   
+    printf("hello world\n");
+    return 0;
+}    
--- miles-km.c
+++ miles-km.c
@@ -7,13 +7,17 @@
 
 int main (void) {
     // Declaración con inicialización
-    int miles,yards=385;
+    int miles,yards=385, ejemplo;
     // Declaración
     double kilometers;
 
     printf("Insert the marathon's distance in miles: ");
     scanf("%d", &miles);
+    // Es importante tener en cuenta que, al dividir dos enteros en C que no se pueden expresar como un entero, obtendremos el entero más próximo
+    // Si se divide un entero y un número decimal (punto flotante como float o double) el resultado es un número decimal
     kilometers = 1.609*(miles + yards/1760.0);
     printf("\n A marathon is %lf kilometers.\n", kilometers);
+    ejemplo = 4/3;
+    printf("\nEste es el ejempĺo: %d\n", ejemplo);
     return 0;
 }
