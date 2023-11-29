1dc8a7ec1b725fed20777ad6441e34a40bad1a8d
helliotroa <helliotroa@cihologramas.com>
2023-11-29 15:13:13 -0500

    Se agrega script de poema - tarea semana 1

---
+++ homework/poem-week1.c
@@ -0,0 +1,27 @@
+/*
+    Printing a poem
+    Johann Roa
+    November, 2023
+*/
+
+#include<stdio.h>
+
+
+int main(void) {
+    printf(
+        "*****************************************************************************************\n"
+        "No te amo como si fueras rosa de sal, topacio o flecha de claveles que propagan el fuego:\n"
+        "te amo como se aman ciertas cosas oscuras, secretamente, entre la sombra y el alma.\n"
+        "Te amo como la planta que no florece y lleva oculta dentro de sí, escondida, la luz de aquellas flores,\n"
+        "y gracias a tu amor vive oscuro en mi cuerpo el apretado aroma que ascendió de la tierra.\n"
+        "Te amo sin saber cómo, ni cuándo, ni de dónde,\n"
+        "te amo directamente sin problemas ni orgullo:\n"
+        "así te amo porque no sé amar de otra manera, \n"
+        "sino así de este modo en que no soy ni eres,\n"
+        "tan cerca que tu mano sobre mi pecho es mía, \n"
+        "tan cerca que se cierra mis ojos con tu sueño.\n"
+        "********************************************************************************************************\n"
+        "Soneto XVII - Pablo Neruda.\n"
+    );
+    return 0;
+}
