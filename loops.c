cd8e320a376ed0dfb1bd89b43a5bc4645ac2c4ea
helliotroa <helliotroa@cihologramas.com>
2024-01-15 10:15:42 -0500

    Casos interesantes de la sintaxis del ciclo For en C

---
+++ loops.c
@@ -0,0 +1,44 @@
+/*
+    Detalles particulares de los loop en C
+    January, 2024
+*/
+
+#include <stdio.h>
+
+int main (void) {
+    int i;
+    // Ciclo infinito, no cambia el índice nunca y no se inicializa i
+    // for (;i<=10;) {
+    //     printf("Este es el índice %d \n", i);
+    // };
+
+    /*
+        Ciclo normal. El riesgo esta en que no se puede garantizar el valor que toma i, 
+        parece inicializar en cero, pero hay un riesgo latente de que empiece con otro valor, 
+        depende del compilador en todo caso.
+    */ 
+    // for (;i<=10;i++) {
+    //     printf("Este es el índice %d \n", i);
+    // };
+
+    // Ciclo infinito, no cambia el índice nunca
+    // for (i=1;i<=10;) {
+    //     printf("Este es el índice %d \n", i);
+    // };
+
+    // Ciclo infinito, se inicializa el índice y cambia su valor pero no hay criterio de parada
+    // for (i=0;;i++){
+    //     printf("Este es el índice %d \n", i);
+    // };
+
+    //Ciclo infinito, el índice no cambia y tampoco se inicializa. Puede ser útil pero sería más compacto un while
+    // for (;;) {
+    //     printf("Este es el índice %d \n", i);
+    // };
+
+    //Ciclo normal, se inicializa el índice, cambia y se define un criterio de parada
+    for (i=0; i<=10; i++){
+        printf("Este es el índice %d \n", i);
+    };
+    return 0;
+}
